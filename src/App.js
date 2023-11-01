708a8ddda69c77a001e22bf2201bb9277116cb56
Siva Srinivasa Rao Mothukuri <sivagtr@live.com>
2023-11-02 02:04:35 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
